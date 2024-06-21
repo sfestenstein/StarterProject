318210bc1b71ff789f8a53e03e6ebe414dc3e054
Steven Festenstein <steven.festenstein@yahoo.com>
2024-06-21 16:03:46 -0400

    Adding General Purpose Logging and CommonUtils folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <signal.h>
+#include <thread>
+
+#include "CommonUtils/GeneralLogger.h"
+
+void sigHandler(int signalNumber)
+{
+    GPCRIT("Signal Caught! {}", signalNumber);
+
+    CommonUtils::GeneralLogger::traceLogger->dump_backtrace();
+}
+
 
 int main()
 {
-    std::cout << "My Starter Project" << std::endl;
+    // Catch seg faults for logging purposes
+    // and initialize logger.  this should alway
+    // be done as soon as possible!
+    signal(SIGSEGV, sigHandler);
+    CommonUtils::GeneralLogger logObject;
+    logObject.init("StarterProj");
+    
+    GPTRACE("Trace Test!");
+    GPINFO("Hello Starter Project!");
     return 0;
 }
